6922d2a8e4c5c2d55fe4d973373444fe5d10cb2c
Evans Munsha <evansensteen@gmail.com>
2025-12-30 19:54:51 +0200

    dist

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'dist',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
@@ -12,4 +20,4 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig)
